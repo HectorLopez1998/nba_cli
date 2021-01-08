291672205359f3d13f08663014b4eac74b204a2d
HectorLopez1998 <hectorlopezp1998@gmail.com>
2021-01-08 16:33:46 -0500

    added greet method to cli

--- lib/nba_cli/cli.rb
+++ lib/nba_cli/cli.rb
@@ -23,6 +23,12 @@ class CLI
             gets.strip
         end
 
+        def greet(name)
+            puts "Cool name! #{name}, would you like me to show you a list of the teams that play in the NBA?."
+            puts "Input 'y' to see the list, 'exit' to decline"
+            menu
+        end
+
         def menu
             selection = user_input
             if selection == 'y'
