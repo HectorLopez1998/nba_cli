b348693ae1fe4410f028ea2eaed5387aa4fa5b48
HectorLopez1998 <hectorlopezp1998@gmail.com>
2021-01-08 16:45:52 -0500

    add initialize mehtod to NBA

--- lib/nba_cli/nba.rb
+++ lib/nba_cli/nba.rb
@@ -4,4 +4,11 @@ class NBA
     @@all = []
     attr_accessor :full_name, :abbreviation, :city, :division
 
+    def initialize(teams_hash)
+        teams_hash.each do |k, v|
+            self.send("#{k}=", v) if self.respond_to?("#{k}=")
+        end
+        save
+    end
+
 end
