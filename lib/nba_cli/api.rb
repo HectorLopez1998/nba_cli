fc008f0cccfd3141d4b8ec1987031fa1ef500f46
HectorLopez1998 <hectorlopezp1998@gmail.com>
2021-01-08 16:41:05 -0500

    updated self.get_data inside api class

--- lib/nba_cli/api.rb
+++ lib/nba_cli/api.rb
@@ -4,7 +4,7 @@ class API
 
     def self.get_data
         response = RestClient.get('https://www.balldontlie.io/api/v1/teams')
-        vehicles_array = JSON.parse(response)["data"]
+        teams_array = JSON.parse(response)["data"]
     end
 
 end
